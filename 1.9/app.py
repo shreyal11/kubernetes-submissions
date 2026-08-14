a3ef01132d610541e7b625c06e77400d545cd679
Shreya Lokhande <shreyalokhande1412@gmail.com>
2026-08-15 01:17:20 +0530

    Complete exercise 1.9

---
+++ 1.9/app.py
@@ -0,0 +1,30 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+counter = 0
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        global counter
+
+        if self.path == "/pingpong":
+            response = f"pong {counter}\n"
+            counter += 1
+
+            self.send_response(200)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(response.encode())
+        else:
+            self.send_response(404)
+            self.end_headers()
+
+    def log_message(self, format, *args):
+        return
+
+
+server = HTTPServer(("0.0.0.0", 8000), Handler)
+
+print("Ping-pong server started in port 8000", flush=True)
+
+server.serve_forever()
